90f8b349918f40ddf0196ecba15911827ab7d1a7
nonomit <amitpal6464@gmail.com>
2026-09-20 02:16:58 +0530

    make title bar 27pix

--- src/Tabs.h
+++ src/Tabs.h
@@ -1,7 +1,7 @@
 /* Copyright 2022 the SumatraPDF project authors (see AUTHORS file).
    License: GPLv3 */
 
-constexpr int kTabBarDy = 26;
+constexpr int kTabBarDy = 27;
 
 int GetTabbarHeight(HWND, float factor = 1.f);
 
