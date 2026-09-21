5a299aa9d709cda3c7f6c35ee74e2e112316ed07
nonomit <amitpal6464@gmail.com>
2026-09-21 21:50:54 +0530

    update home

--- src/SvgIcons.cpp
+++ src/SvgIcons.cpp
@@ -324,15 +324,6 @@ const char* gIconHomeThumbnails =
   <rect x="14" y="14" width="6" height="6" rx="1" />
 </svg>)";
 
-// https://github.com/tabler/tabler-icons/blob/main/icons/outline/home.svg
-const char* gIconHome =
-    R"(<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" stroke-width="1" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-  <path stroke="none" d="M0 0h24v24H0z"/>
-  <path d="M5 12l-2 0l9 -9l9 9l-2 0" />
-  <path d="M5 12v7a2 2 0 0 0 2 2h10a2 2 0 0 0 2 -2v-7" />
-  <path d="M9 21v-6a2 2 0 0 1 2 -2h2a2 2 0 0 1 2 2v6" />
-</svg>)";
-
 // https://github.com/tabler/tabler-icons/blob/main/icons/outline/pin.svg
 const char* gIconPin =
     R"(<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" stroke-width="1" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
@@ -672,4 +663,13 @@ const char* gIconArrowUp =
   <path d="M12 5l0 14" />
   <path d="M18 11l-6 -6" />
   <path d="M6 11l6 -6" />
+</svg>)";
+
+// https://github.com/tabler/tabler-icons/blob/main/icons/outline/home.svg
+const char* gIconHome =
+    R"(<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" stroke-width="1" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+  <path stroke="none" d="M0 0h24v24H0z"/>
+  <path d="M5 12l-2 0l9 -9l9 9l-2 0" />
+  <path d="M5 12v7a2 2 0 0 0 2 2h10a2 2 0 0 0 2 -2v-7" />
+  <path d="M9 21v-6a2 2 0 0 1 2 -2h2a2 2 0 0 1 2 2v6" />
 </svg>)";
